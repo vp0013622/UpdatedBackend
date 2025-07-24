c95c039d507e187471b03a5c46ddcdd6e0b58f23
VIJAY PATIL <vijaypatil@VIJAYs-MacBook-Air.local>
2025-07-24 23:35:43 +0530

    Update meeting schedule routes for production release

--- Routes/meetingScheduleRoutes.js
+++ Routes/meetingScheduleRoutes.js
@@ -3,11 +3,11 @@ import { Create, GetAllMeetingSchedules, GetMyMeetings, GetAllNotPublishedMeetin
 import { RoleAuthMiddleware } from '../Middlewares/RoleAuthMiddelware.js'
 
 const MeetingScheduleRouter = express.Router()
-MeetingScheduleRouter.post('/create', RoleAuthMiddleware("admin", "sales", "executive"), Create) //admin, sales, executive
-MeetingScheduleRouter.get('/', RoleAuthMiddleware("admin"), GetAllMeetingSchedules)//public access
+MeetingScheduleRouter.post('/create', RoleAuthMiddleware("admin", "sales", "executive"), Create) //admin, sales, executive can create meeting schedule
+MeetingScheduleRouter.get('/', RoleAuthMiddleware("admin", "executive"), GetAllMeetingSchedules)//public access to get all meeting schedules
 MeetingScheduleRouter.get('/my-meetings/:id', RoleAuthMiddleware("admin", "sales", "executive", "saller", "user"), GetMyMeetings)//my meetings
 MeetingScheduleRouter.get('/notpublished', RoleAuthMiddleware("admin"), GetAllNotPublishedMeetingSchedules)//only for admin
-MeetingScheduleRouter.get('/:id', RoleAuthMiddleware("admin", "sales", "executive", "saller", "user"), GetMeetingScheduleById)//public access
-MeetingScheduleRouter.put('/edit/:id', RoleAuthMiddleware("admin", "sales", "executive"), Edit)//admin, sales, executive
+MeetingScheduleRouter.get('/scheduledByUserId/:id', RoleAuthMiddleware("admin", "sales", "executive"), GetMeetingScheduleById)//public access to get meeting schedule by id
+MeetingScheduleRouter.put('/edit/:id', RoleAuthMiddleware("admin", "sales", "executive"), Edit)//admin, sales, executive can edit meeting schedule
 MeetingScheduleRouter.delete('/delete/:id', RoleAuthMiddleware("admin", "sales", "executive"), DeleteById)//admin, sales, executive
 export default MeetingScheduleRouter 
