958431c6d5d981b3a94eb1928433353cde792cfa
Rushikesh Gaware <gawarerishi@gmail.com>
2026-03-12 09:50:36 +0530

    dd

--- check_meeting_statuses.js
+++ check_meeting_statuses.js
@@ -1,13 +1,13 @@
 
 import mongoose from 'mongoose';
-import {MeetingScheduleStatusModel} from './Models/MeetingScheduleStatusModel.js';
+import { MeetingScheduleStatusModel } from './Models/MeetingScheduleStatusModel.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function run() {
     try {
         await mongoose.connect(process.env.DB_CONNECTION_STRING);
-        const statuses = await MeetingScheduleStatusModel.find({published: true});
+        const statuses = await MeetingScheduleStatusModel.find({ published: true });
         console.log('MEETING_STATUSES:', JSON.stringify(statuses, null, 2));
         process.exit(0);
     } catch (err) {
