a5401b1ba6de154f25acd63bf588e9c777422cea
Rushikesh Gaware <gawarerishi@gmail.com>
2026-04-01 09:29:55 +0530

    feat: add sales role to lead creation authorization middleware

--- Routes/leadsRoutes.js
+++ Routes/leadsRoutes.js
@@ -4,7 +4,7 @@ import { RoleAuthMiddleware } from '../Middlewares/RoleAuthMiddelware.js'
 
 const LeadsRouter = express.Router()
 // Authenticated endpoint for admin/executive
-LeadsRouter.post('/create',  RoleAuthMiddleware("admin", "executive"), Create)
+LeadsRouter.post('/create',  RoleAuthMiddleware("admin", "executive", "sales"), Create)
 LeadsRouter.get('/',  RoleAuthMiddleware("admin", "executive", "sales"), GetAllLeads)
 LeadsRouter.get('/notpublishedusers',  RoleAuthMiddleware("admin", "executive", "sales"), GetAllNotPublishedLeads)
 LeadsRouter.get('/assigned-to-me',  RoleAuthMiddleware("admin", "executive", "sales"), GetAssignedLeadsForCurrentUser)
